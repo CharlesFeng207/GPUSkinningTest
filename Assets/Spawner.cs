dbe19f47847c90798ac1fc8e68ce834a3e1e85fb
fengchao <fengchao@nibirutech.com>
2023-06-11 22:44:26 +0800

    1

---
+++ Assets/Spawner.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    public GameObject Prefab;
+    public int Count;
+    public float Range;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        for(int i = 0; i < Count;i ++)
+        {
+            var go = Instantiate(Prefab, transform); 
+            go.transform.localPosition = Random.insideUnitSphere * Range;
+            go.transform.localRotation = Random.rotation;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
