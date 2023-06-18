8b0413aeaf0346024489e7347af48f01e0cee046
fengchao <fengchao@nibirutech.com>
2023-06-19 01:35:04 +0800

    1

--- Assets/Editor/BuildAll.cs
+++ Assets/Editor/BuildAll.cs
@@ -1,23 +1,31 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
+using System.Text;
 using UnityEditor;
 using UnityEngine;
+using Debug = UnityEngine.Debug;
 
 public static class BuildAll
 {
     public static Tester Tester => GameObject.FindObjectOfType<Tester>();
-    [MenuItem("BuildAll/Build")]
-    public static void Build()
+    private static List<string> Builds = new List<string>();
+
+    [MenuItem("BuildAll/Start &B")]
+    public static void Start()
     {
+        Builds.Clear();
+
         Tester.Count = 1000;
-        Tester.Prefab = AssetDatabase.LoadAssetAtPath<GameObject>("Assets/Adam.prefab");     
-        PlayerSettings.gpuSkinning = false;           
+        Tester.Prefab = AssetDatabase.LoadAssetAtPath<GameObject>("Assets/Adam.prefab");
+        PlayerSettings.gpuSkinning = false;
         Build("Adam-CPUSkin");
-        PlayerSettings.gpuSkinning = true;           
+        PlayerSettings.gpuSkinning = true;
         Build("Adam-ComputeSkin");
 
-        Tester.Prefab = AssetDatabase.LoadAssetAtPath<GameObject>("Assets/AdamGPU.prefab");     
+        Tester.Prefab = AssetDatabase.LoadAssetAtPath<GameObject>("Assets/AdamGPU.prefab");
         Tester.EnableInstancing = false;
         Build("Adam-GPUSkin");
         Tester.EnableInstancing = true;
@@ -26,24 +34,65 @@ public static class BuildAll
         Tester.Count = 100;
         Tester.Prefab = AssetDatabase.LoadAssetAtPath<GameObject>("Assets/Mannequin.prefab");
         PlayerSettings.gpuSkinning = false;
-        Build("Mannequin-CPUSkin");
+        // Build("Mannequin-CPUSkin");
         PlayerSettings.gpuSkinning = true;
-        Build("Mannequin-ComputeSkin");
+        // Build("Mannequin-ComputeSkin");
 
         Tester.Prefab = AssetDatabase.LoadAssetAtPath<GameObject>("Assets/MannequinGPU.prefab");
         Tester.EnableInstancing = false;
-        Build("Mannequin-GPUSkin");
+        // Build("Mannequin-GPUSkin");
         Tester.EnableInstancing = true;
-        Build("Mannequin-GPUSkin-Batched");
+        // Build("Mannequin-GPUSkin-Batched");
+
+        RunTest();
     }
 
     private static void Build(string name)
     {
-        EditorUtility.SetDirty(AssetDatabase.LoadAssetAtPath<Object>("Assets/TestSkinning.unity"));
+        EditorUtility.SetDirty(AssetDatabase.LoadAssetAtPath<UnityEngine.Object>("Assets/TestSkinning.unity"));
         AssetDatabase.SaveAssets();
         AssetDatabase.Refresh();
-        PlayerSettings.applicationIdentifier = "com.gpuskintest." + name.ToLower();
+        PlayerSettings.applicationIdentifier = "com.gpuskin.test";
         PlayerSettings.productName = name;
         BuildPipeline.BuildPlayer(new[] { "Assets/TestSkinning.unity" }, Path.Combine("Build", name + ".apk"), BuildTarget.Android, BuildOptions.Development);
+        Builds.Add(name);
     }
+
+    private static void RunTest()
+    {
+        RunCmd($"python RunTest.py {string.Join(";", Builds)}");
+    }
+
+    private static void RunCmd(string cmd)
+    {
+        // Create a new process
+        Process process = new Process();
+
+        // Set the process start info
+        ProcessStartInfo startInfo = new ProcessStartInfo();
+        startInfo.FileName = "cmd.exe";
+        startInfo.Arguments = "/c " + cmd;
+        startInfo.UseShellExecute = false;
+        startInfo.RedirectStandardOutput = true;
+
+        // Start the process
+        process.StartInfo = startInfo;
+        process.Start();
+
+        // Read the output and display it
+        StringBuilder output = new StringBuilder();
+        while (!process.StandardOutput.EndOfStream)
+        {
+            string line = process.StandardOutput.ReadLine();
+            output.AppendLine(line);
+        }
+
+        // Close the process
+        process.WaitForExit();
+        process.Close();
+
+        // Display the captured output
+        Debug.Log(output.ToString());
+    }
+
 }
---
+++ RunTest.py
@@ -0,0 +1,25 @@
+import os
+import sys
+from time import sleep
+
+def test(pkgname):
+    os.system("adb uninstall com.gpuskin.test")
+    script_directory = os.path.dirname(os.path.abspath(__file__))
+    pkgpath = os.path.join(script_directory, "Build", pkgname + ".apk")
+    os.system("adb install " + pkgpath)
+    run()
+    pass
+
+def run():
+    os.system("adb shell am start -n com.gpuskin.test/com.gpuskin.test.MainActivity")
+    sleep(5)
+    os.system("adb shell am force-stop com.gpuskin.test")
+    pass
+
+if __name__ == "__main__":
+    print(sys.argv[1])
+    buids = sys.argv[1].split(";")
+    for build in buids:
+        test(build)
+    input("Press Enter to continue...")
+    pass
